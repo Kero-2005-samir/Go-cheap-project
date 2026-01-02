b8f7b20a514d06e294d9eb832a9d3823de3d3625
Kero-2005-samir <kerolous2005@gmail.com>
2026-01-02 05:12:04 +0200

    Add files via upload

---
+++ javaScript/script.js
@@ -0,0 +1,30 @@
+const container = document.querySelector(".container");
+
+function showRegister() {
+  container.classList.add("active");
+}
+
+function showLogin() {
+  container.classList.remove("active");
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+    const preloader = document.getElementById('preloader');
+    preloader.style.opacity = "0";
+    preloader.style.top = "-100vh";
+
+    setTimeout(() => {
+      preloader.style.display = "none";
+    }, 800);
+  }, 2000);
+});
+
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault(); 
+  window.location.href = "home.html";
+});
+document.getElementById("registerForm").addEventListener("submit", function (e) {
+  e.preventDefault(); 
+  window.location.href = "home.html";
+});
